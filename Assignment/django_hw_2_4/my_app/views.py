ea4c8def7e51c0a69c8208ce27f919f9807647c1
YJ <eu9309@naver.com>
2026-04-15 10:06:41 +0900

    0415

---
+++ Assignment/django_hw_2_4/my_app/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+def introduce(request, username):
+    context = {'username': username}
+    return render(request, 'my_app/introduce.html', context)
